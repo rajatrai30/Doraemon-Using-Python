7999fcf343630898af30226e5337f48384fcb8ab
Rajat Rai <75417243+R986250@users.noreply.github.com>
2021-08-16 13:58:06 +0530

    Add files via upload

---
+++ dd.py
@@ -0,0 +1,18 @@
+from turtle import *
+from doraemon import *
+pensize(3)
+speed(10)
+head()
+belt()
+face()
+nose()
+mouth()
+whisker()
+body()
+arms()
+pocket()
+bell()
+black_eyes()
+ht()
+done()
+                                   
